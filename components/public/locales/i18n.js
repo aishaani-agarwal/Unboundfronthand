3f1818ff3adff1f216ce1336072cc0812b2267e4
aishaani-agarwal <aishaani@gmail.com>
2024-01-23 17:27:33 +0530

    this should work now

--- components/public/locales/i18n.js
+++
@@ -1,8 +0,0 @@
-// i18n.js
-const NextI18Next = require("next-i18next").default;
-
-module.exports = new NextI18Next({
-  defaultLanguage: "en",
-  otherLanguages: ["hi", "ta", "kn"],
-  localePath: "public/locales",
-});
